40506f84bd1c38c879ca528ac4b701b9113e1939
Dylan Pinn <me@dylanpinn.com>
2019-11-19 12:14:08 +1100

    Print CLI instructions

--- toy_robot.rb
+++ toy_robot.rb
@@ -11,6 +11,18 @@ tabletop = Tabletop.new
 robot = Robot.new
 driver = CLIDriver.new(tabletop, robot)
 
+# Print Instructions
+p 'Toy Robot'
+p '---------'
+p 'Operations:'
+p 'PLACE X, Y, DIRECTION  - Place the robot on the board'
+p 'Other operations are ignored unless the robot has been placed on the board'
+p 'MOVE - Move robot forward one place'
+p 'LEFT - Rotate robot left 90 degrees'
+p 'RIGHT - Rotate robot right 90 degrees'
+p 'REPORT - Return the robot\'s position and direction'
+p '<CTRL-c> - Quit the program'
+
 while (buf = Readline.readline(prompt, true))
   driver.parse(buf.downcase)
 end
